77db1a8af738ec4d1000ef51d19701cfa8a74466
mouad ayoub <mouadayoubmouadayoub@gmail.com>
2025-02-27 21:42:08 +0000

    styling the update and deleter forme

--- resources/js/Layouts/AuthenticatedLayout.tsx
+++ resources/js/Layouts/AuthenticatedLayout.tsx
@@ -48,8 +48,8 @@ export default function Authenticated({
       </nav>
 
       {header && (
-        <header className="bg-white shadow">
-          <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+        <header className="bg-background shadow">
+          <div className="mx-auto max-w-7xl p-4 sm:p-6">
             {header}
           </div>
         </header>
