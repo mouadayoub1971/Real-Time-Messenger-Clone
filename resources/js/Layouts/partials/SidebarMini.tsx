e3b6ed8117d07694e7b80bae00569f098b5e6fe7
mouad ayoub <mouadayoubmouadayoub@gmail.com>
2025-04-15 19:46:57 +0100

    side bar of chats

--- resources/js/Layouts/partials/SidebarMini.tsx
+++ resources/js/Layouts/partials/SidebarMini.tsx
@@ -4,7 +4,7 @@ import BadgeNotification from "@/components/chats/BadgeNotification";
 export default function SidebarMini() {
   return (
     <div className="order-2 flex flex-col justify-center items-center">
-      <Link href={route("chats.index")} className="relative flex flex-1 items-center justify-center rounded-lg p-3 transition-all hover:bg-secondary sm:flex-initial">
+      <Link href={route("chats.index")} className="">
         <BsChat className="h-6 w-6" />
         <BadgeNotification/>
       </Link>
